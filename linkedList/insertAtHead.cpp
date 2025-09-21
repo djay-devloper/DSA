7a0259b0160d8790dd307626ff946658e75fb56c
djay-devloper <djay.developer@gmail.com>
2025-09-21 10:58:10 +0530

    insert at head

---
+++ linkedList/insertAtHead.cpp
@@ -0,0 +1,43 @@
+//there are two cases either list is empty or a given list with adress of its head.
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int data){
+        this->data= data;
+        next= NULL;
+    }
+};
+void insertAtHead(Node* &head, int d){
+    Node*newNode= new Node(d);
+    //if list is empty then only update head.
+    if(head== NULL){
+        head= newNode; 
+        return;
+    }
+    //some nodes are already present 
+    else{
+        newNode->next= head;
+        head= newNode;  
+    }
+
+}
+void printList(Node* &head){
+    Node*itr= head;
+    while(itr!= NULL){
+        cout<<itr->data<<"->";
+        itr= itr->next;
+    }
+    cout<<"NULL";
+}
+int main(){
+    Node*head= nullptr;
+    insertAtHead(head, 10);
+    insertAtHead(head, 20);
+    insertAtHead(head, 30);
+    printList(head);
+    return 0;
+}
